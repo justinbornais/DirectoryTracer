6d48361dec06603076ef2aee43fb6354e29b7f07
Justin Bornais <justin.bornais@gmail.com>
2025-02-05 14:26:08 -0500

    Update tracer.py

--- tracer.py
+++ tracer.py
@@ -8,10 +8,13 @@ def read_file_to_string(file_path):
     file_contents = re.sub(r'/\*.*?\*/', '', file_contents, flags=re.DOTALL)
     file_contents = ''.join(file_contents.splitlines())
     file_contents = file_contents.replace("\t", "").replace("  ", "")
-    if file_path.endswith(".css"):
-        file_contents = file_contents.replace(" ", "")
     return file_contents
 
+def split_path(path):
+    parts = path.lstrip('./').split('/')
+    return [part for part in parts if part]
+
+title = "Directory Tracer"
 css = read_file_to_string("./directoryStyles.css")
 js = read_file_to_string("./directoryScript.js")
 
@@ -21,65 +24,72 @@ if os.path.exists('.fileignore'):
     ignored = set(open('.fileignore', 'r').read().split('\n') + ignored)
 
 # boilerplate used in the output html file
-boilerplate1 = f"""<html>
+boilerplate = f"""<html>
 <head>
-    <title>Site Title</title>
+    <title>{title}</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width">
     <link rel="icon" href="data:;base64,iVBORw0KGgo=">
     <style>{css}</style>
 </head>
 <body>
-    <h2>View Directory Contents</h2>
-    <div class="i"><input type="text" class="q" id="q" placeholder="Search for a file" /></div>
-    """
-
-boilerplate2 = f"""    </ul>
+    <div id="top"><h1>[parent]<a class="n" href="[base]">{title}</a>[dir]</h1>
+        <input type="text" class="q" id="q" placeholder="Search for a file" />
+    </div>
+    <ul id="dl"></ul>
     <script src="https://cdn.jsdelivr.net/npm/fuse.js@6.6.2"></script>
-    <script>{js}</script>\n</body>\n</html>"""
+<script>[data]{js}</script>\n</body>\n</html>"""
 
 # recursive function that writes index.html and calls indexFolder for subdirectories
-def indexFolder(directory):
+def indexFolder(directory, boilerplate, depth):
     print(f"Current directory: {directory}")
+
+    bcopy = boilerplate
+    base = "../" * depth
+    boilerplate = boilerplate.replace("[base]", base)
+
     folder_list = [name for name in os.listdir(directory) if os.path.isdir(os.path.join(directory, name))] # Source: https://www.codegrepper.com/code-examples/python/get+list+of+folders+in+directory+python
     folder_list.sort()
     if len(folder_list) > 0:
         for i in range(len(folder_list)):
             if folder_list[i] not in ignored and not folder_list[i][0] == '.':
-                indexFolder(os.path.join(directory, folder_list[i]))
+                indexFolder(os.path.join(directory, folder_list[i]), bcopy, depth + 1)
 
     file_list = [n for n in os.listdir(directory) if not n in folder_list] # anything not in folder_list is a file
     file_list.sort()
     jsData = ""
-
-    f = open(str(directory) + "/index.html", "w", encoding="utf8") # Create the file.
     
-    # Fill with initial HTML code.
-    f.write(boilerplate1)
+    # Add the folder path and a link to the parent page.
+    title_string = ""
+    parent_string = ""
     if directory != ".":
-        f.write("   <h1>" + directory.replace("\\", "/") + "</h1>\n")
-        f.write("        <li class=\"d\"><b><a href=\"..\">⬆ Parent</a></b></li></br>\n")
-        
-    f.write("    <ul id=\"dl\">\n")
+        dir_string = directory.replace("\\", "/")
+        parts = split_path(dir_string)
+        title_string = " - "
+        for i in range(len(parts)):
+            depth = "../" * (len(parts) - i)
+            title_string += f"<a class=\"n\" href=\"{depth}{parts[i]}\">{parts[i]}</a>"
+            if i != len(parts) - 1: title_string += "/"
+       
+        parent_string = '<b><a href=".." class="p">←</a></b>'
+    boilerplate = boilerplate.replace('[dir]', title_string).replace('[parent]', parent_string)
     
     # Write folder contents.    
     for i in range(len(folder_list)):
         if folder_list[i] not in ignored and not folder_list[i][0] == '.':
             jsData += f'{{n:"{folder_list[i]}",t:"d"}},'
 
-    # Separate folders from files.
-    f.write("        <br />\n")
-
     # Write file contents.
     for i in range(len(file_list)):
         if file_list[i] not in ignored and file_list[i] != "index.html" and not file_list[i][0] == '.':
             jsData += f'{{n:"{file_list[i]}",t:"f"}},'
     
-    boilerplate3 = boilerplate2.replace("const data = [];", f"const data = [{jsData}];")
+    boilerplate = boilerplate.replace("[data]", f"const d = [{jsData}];")
     
     # End off the html file.
-    f.write(boilerplate3)
+    f = open(str(directory) + "/index.html", "w", encoding="utf8") # Create the file.
+    f.write(boilerplate)
     f.close()
 
 # Base case, where we index the current folder.
-indexFolder(os.curdir)
+indexFolder(os.curdir, boilerplate, 0)
